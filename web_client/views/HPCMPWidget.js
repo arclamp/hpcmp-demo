f603eb58a2d8e83486bf09e02672401063bdaba1
Roni Choudhury <roni.choudhury@kitware.com>
2019-01-21 13:36:52 -0500

    Use start/stop buttons to control playback

--- web_client/views/HPCMPWidget.js
+++ web_client/views/HPCMPWidget.js
@@ -1,22 +1,24 @@
 import { LineChart } from '@candela/vega';
 
 import View from 'girder/views/View';
+import { restRequest } from 'girder/rest';
 
 import HPCMPWidgetTemplate from './hpcmpWidget.pug';
 
 const HPCMPWidget = View.extend({
   events: {
     'click button.start': function (event) {
-      this.running = true;
+      this.start();
     },
     'click button.stop': function (event) {
-      this.running = false;
+      this.stop();
     },
   },
 
   initialize: function (settings) {
     this.item = settings.item;
     this.accessLevel = settings.accessLevel;
+    this.interval = null;
 
     this.render();
   },
@@ -36,12 +38,41 @@ const HPCMPWidget = View.extend({
     });
     this.volumeChart.render();
 
+    this.start();
+
     return this;
+  },
+
+  start: function () {
+    if (!this.interval) {
+      this.interval = window.setInterval(() => this.getData(), 1000);
+    }
+  },
+
+  stop: function () {
+    if (this.interval) {
+      window.clearInterval(this.interval);
+      this.interval = null;
+    }
+  },
+
+  getData: function () {
+    restRequest({
+      type: 'POST',
+      url: `hpcmp/stream/${this.item.id}/read`,
+    }).then((data) => {
+      console.log(data.data);
+      let rec = {
+        a: +data.data[0].ts,
+        b: +data.data[0].resp_bytes || 0
+      };
+      console.log(rec);
+      this.addData(rec.a, rec.b);
+    });
   },
 
   addData: function (x, y) {
     this.volumeChart.options.data.push({a: x, b: y});
-    // this.volumeChart.options.data = this.volumeChart.options.data.slice(1);
     this.volumeChart.render();
   }
 });
--- web_client/views/ItemView.js
+++ web_client/views/ItemView.js
@@ -1,6 +1,5 @@
 import ItemView from 'girder/views/body/ItemView';
 import { wrap } from 'girder/utilities/PluginUtils';
-import { restRequest } from 'girder/rest';
 
 import HPCMPWidget from './HPCMPWidget';
 
@@ -18,43 +17,6 @@ wrap(ItemView, 'render', function (render) {
         parentView: this
       });
       this.hpcmpWidget.$el.appendTo(this.$el);
-
-      restRequest({
-        type: 'POST',
-        url: `hpcmp/stream/${this.model.id}`,
-        data: {
-          header: true
-        }
-      }).then((post) => {
-        console.log(post);
-
-        const getData = () => {
-          restRequest({
-            type: 'POST',
-            url: `hpcmp/stream/${this.model.id}/read`,
-          }).then((data) => {
-            console.log(data);
-            let rec = {
-              a: +data.data[0].ts,
-              b: +data.data[0].resp_bytes || 0
-            };
-            console.log(rec);
-            this.hpcmpWidget.addData(rec.a, rec.b);
-
-            window.setTimeout(getData, 1000);
-          });
-        }
-
-        getData();
-      });
-
-      // let counter = 1.5;
-      // let value = 0;
-      // window.setInterval(() => {
-        // this.hpcmpWidget.addData(counter, 15 + value);
-        // counter += 0.1;
-        // value = (value + 1) % 5;
-      // }, 1000);
     }
   });
 
