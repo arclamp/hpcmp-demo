79d22541b20cd226d919701aa93aa4cd05752dbf
Roni Choudhury <roni.choudhury@kitware.com>
2019-01-25 13:26:43 -0500

    Use a more careful method of wrapping Girder ItemView

--- web_client/views/ItemView.js
+++ web_client/views/ItemView.js
@@ -6,35 +6,37 @@ import HPCMPWidget from './HPCMPWidget';
 
 wrap(ItemView, 'render', function (render) {
   this.model.getAccessLevel(accessLevel => {
-    render.call(this);
-
     const go = (this.model.get('meta') || {}).brolog;
 
     if (go) {
-      this.hpcmpWidget = new HPCMPWidget({
-        className: 'g-hpcmp-container',
-        item: this.model,
-        accessLevel: accessLevel,
-        parentView: this
-      });
-      this.hpcmpWidget.$el.appendTo(this.$el);
-
-      restRequest({
-        type: 'DELETE',
-        url: `hpcmp/stream/${this.model.id}`
-      }).then(() => {
-        return restRequest({
-          type: 'POST',
-          url: `hpcmp/stream/${this.model.id}`,
-          data: {
-            header: true
-          }
+      this.once('g:rendered', () => {
+        this.hpcmpWidget = new HPCMPWidget({
+          className: 'g-hpcmp-container',
+          item: this.model,
+          accessLevel: accessLevel,
+          parentView: this
         });
-      }).then((post) => {
-        this.hpcmpWidget.start();
+        this.hpcmpWidget.$el.appendTo(this.$el);
+
+        restRequest({
+          type: 'DELETE',
+          url: `hpcmp/stream/${this.model.id}`
+        }).then(() => {
+          return restRequest({
+            type: 'POST',
+            url: `hpcmp/stream/${this.model.id}`,
+            data: {
+              header: true
+            }
+          });
+        }).then((post) => {
+          this.hpcmpWidget.start();
+        });
       });
     }
   });
+
+  render.call(this);
 
   return this;
 });
